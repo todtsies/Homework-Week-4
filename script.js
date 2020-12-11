4bfaa8ae80c003e421e6d56890d94f9305bf7a0c
Emily Todt <72036110+todtsies@users.noreply.github.com>
2020-12-10 21:24:59 -0500

    Finish script.js

--- script.js
+++ script.js
@@ -22,10 +22,11 @@ var questions = [
     },
     {
         title: "Which of the following HTML elements is used to create an unordered list?",
-        choices: ["<ui>", "<li>", "<ul>", "<list>"],
-        answer: "<ul>"
+        choices: ["ui", "li", "ul", "list"],
+        answer: "ul"
     }
 ]
+
 
 // Declaring the numerical values of my score and timer functions
 var score = 0;
@@ -51,4 +52,104 @@ function start() {
     }, 1000);
     
     next();
+}
+
+// The game ends when the timer is stopped
+function endGame() {
+    clearInterval(timer);
+
+    var quizContent = `
+        <h2>Game over!</h2>
+        <h3>You got a ` + score + ` /100!</h3>
+        <h3>That means you got ` + score / 20 + ` questions correct!</h3>
+        <input type="text" id="name" placeholder="Please enter your initals">
+        <button onclick="setScore()">Set score!</button>`;
+
+        document.getElementById("quizBody").innerHTML = quizContent;
+}
+
+function setScore() {
+    localStorage.setItem("highscore", score);
+    localStorage.setItem("highscoreName", document.getElementById('name').value);
+    getScore();
+}
+
+function getScore() {
+    var quizContent = `
+    <h2>` + localStorage.getItem("highscoreName") + `'s highscore is:</h2>
+    <h1>` + localStorage.getItem("highscore") + `</h1><br>
+
+    <button onclick="clearScore()">Clear score!</button><button onclick="resetGame()">Play again!</button>
+    
+    `;
+
+    document.getElementById("quizBody").innerHTML = quizContent;
+}
+
+//Clears the score name and value in the local storage if the user selects "clear score"
+function clearScore() {
+    localStorage.setItem("highscore", "");
+    localStorage.setItem("highscoreName", "");
+
+    resetGame();
+}
+
+//Reset the quiz
+function resetGame() {
+    clearInterval(timer);
+    score = 0;
+    currentQuestion = -1;
+    timeLeft = 0;
+    timer = null;
+
+    document.getElementById("timeLeft").innerHTML = timeLeft;
+
+    var quizContent = `
+    <h1>
+        Coding Quiz!
+    </h1>
+    <h3>
+        Click Start to play!
+    </h3>
+    <button onclick="start()">Start!</button>`;
+
+    document.getElementById("quizBody").innerHTML = quizContent;
+
+}
+
+//Deduct 15 seconds from the timer if user guesses wrong
+function incorrect() {
+    timeLeft -=15;
+    next();
+}
+
+//Increase the score by 20 if the user guesses right
+function correct() {
+    score += 20;
+    next()
+}
+
+function next() {
+    currentQuestion++;
+
+    if (currentQuestion > questions.length - 1) {
+        endGame();
+        return;
+    }
+
+    var quizContent = "<h2>" + questions[currentQuestion].title + "</h2>"
+
+    for (var buttonLoop = 0; buttonLoop < questions[currentQuestion].choices.length; buttonLoop++) {        
+        var buttonCode = "<button onclick=\"[ANS]\">[CHOICE]</button>";         
+        buttonCode = buttonCode.replace("[CHOICE]", questions[currentQuestion].choices[buttonLoop]);        
+        
+        if (questions[currentQuestion].choices[buttonLoop] == questions[currentQuestion].answer) {           
+             buttonCode = buttonCode.replace("[ANS]", "correct()");        
+        }   else { 
+               buttonCode = buttonCode.replace("[ANS]", "incorrect()");       
+             }        
+             quizContent += buttonCode   
+    }
+    
+    document.getElementById("quizBody").innerHTML = quizContent;
 }
